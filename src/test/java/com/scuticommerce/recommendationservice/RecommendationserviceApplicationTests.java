a516e667a779ea3acf9b6454509fe891eff53cb0
Kapil Kataria <kapil.kataria@kp.org>
2019-03-15 20:17:47 -0700

    first commit

---
+++ src/test/java/com/scuticommerce/recommendationservice/RecommendationserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scuticommerce.recommendationservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecommendationserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
